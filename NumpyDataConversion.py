5030416b59f16f7ec453b7c9616b283678d7eba4
Noah Barrett <48027601+NoahBarrett98@users.noreply.github.com>
2021-11-09 10:12:02 -0400

    Update NumpyDataConversion.py

--- NumpyDataConversion.py
+++ NumpyDataConversion.py
@@ -4,30 +4,30 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 import os
+ # = r"C:\Users\conno\ML_Scripts\ML_Project\FETAL_PLANES_ZENODO\FETAL_PLANES_DB_data.xlsx"
+#  = r"C:\Users\conno\ML_Scripts\ML_Project\FETAL_PLANES_ZENODO\Images"
+def NumpyConversion(directoryxlsx,
+                    directoryimg):
 
-directorycsv = r"C:\Users\conno\ML_Scripts\ML_Project\FETAL_PLANES_ZENODO"
-directoryimg = r"C:\Users\conno\ML_Scripts\ML_Project\FETAL_PLANES_ZENODO\Images"
-xlsx = "FETAL_PLANES_DB_data.xlsx"
-xlsxloc = os.path.join(directorycsv, xlsx)
 
-df = pd.read_excel(xlsxloc)
+    df = pd.read_excel(directoryxlsx)
 
-imagenames = []
-imagelocs = []
-imagearrays = []
-planesout = []
+    imagenames = []
+    imagelocs = []
+    imagearrays = []
+    planesout = []
 
-for i, rows in df.iterrows():
-    imgname = df.at[i, 'Image_name'] + '.png'
-    imgloc = os.path.join(directoryimg, imgname)
-    img = Image.open(imgloc)
-    array = np.asarray(img)
-    plane = df.at[i, 'Plane']
-    output = np.asarray(plane)
-    imagenames.append(imgname)
-    imagelocs.append(imgloc)
-    imagearrays.append(array)
-    planesout.append(output)
-    img.close()
+    for i, rows in df.iterrows():
+        imgname = df.at[i, 'Image_name'] + '.png'
+        imgloc = os.path.join(directoryimg, imgname)
+        img = Image.open(imgloc)
+        array = np.asarray(img)
+        plane = df.at[i, 'Plane']
+        output = np.asarray(plane)
+        imagenames.append(imgname)
+        imagelocs.append(imgloc)
+        imagearrays.append(array)
+        planesout.append(output)
+        img.close()
 
-print(imagearrays[5])
+    return imagenames, imagelocs, imagearrays, planesout
